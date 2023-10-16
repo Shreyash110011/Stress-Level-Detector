ada950e17207289f184eff008350039b72544dcb
Shreyash110011 <147143774+Shreyash110011@users.noreply.github.com>
2023-10-16 22:49:41 +0530

    Update web_functions.py

--- web_functions.py
+++ web_functions.py
@@ -51,3 +51,16 @@ def predict(X, y, features):
     prediction = model.predict(np.array(features).reshape(1, -1))
 
     return prediction, score
+
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
